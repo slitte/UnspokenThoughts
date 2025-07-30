fe18d0da35cb8156539b265e3afcce4674a433c0
slitte <84241164+slitte@users.noreply.github.com>
2025-07-30 13:14:29 +0200

    Update port_handler.rs

--- software/backend/src/port_handler.rs
+++ software/backend/src/port_handler.rs
@@ -1,10 +1,11 @@
 use tokio_serial::{SerialPortBuilderExt, SerialStream};
 use tokio_util::codec::{FramedRead, LinesCodec};
 use tokio_stream::StreamExt;
-use tokio::io::AsyncWriteExt;
+use tokio::io::{AsyncWriteExt, BufWriter};
 use crate::message::{MeshMessage, PortMessage};
 use crate::event::Event;
 use tokio::sync::mpsc::UnboundedSender;
+use tokio::time::{sleep, Duration};
 
 pub struct PortHandler {
     port_name: String,
@@ -23,21 +24,47 @@ impl PortHandler {
             {
                 Ok(port) => {
                     log::info!("[{}] Verbunden", self.port_name);
-                    if let Err(e) = self.read_loop(port).await {
+
+                    // Aufteilen in Reader + Writer
+                    let (reader, writer) = tokio::io::split(port);
+                    let framed = FramedRead::new(reader, LinesCodec::new());
+                    let mut writer = BufWriter::new(writer);
+
+                    // Task für NodeInfo-Anfragen starten
+                    let mut writer_clone = writer.clone();
+                    let port_name_clone = self.port_name.clone();
+                    tokio::spawn(async move {
+                        let mut interval = tokio::time::interval(Duration::from_secs(600));
+                        loop {
+                            interval.tick().await;
+                            let cmd = b"{\"request\": \"node_info\"}\n";
+                            match writer_clone.write_all(cmd).await {
+                                Ok(_) => log::info!("[{}] NodeInfo angefragt", port_name_clone),
+                                Err(e) => log::warn!("[{}] Fehler bei NodeInfo-Schreiben: {:?}", port_name_clone, e),
+                            }
+                            if writer_clone.flush().await.is_err() {
+                                break;
+                            }
+                        }
+                    });
+
+                    // Hauptleseschleife
+                    if let Err(e) = self.read_loop(framed).await {
                         log::warn!("[{}] Lesefehler: {:?}", self.port_name, e);
                     }
                 }
                 Err(e) => {
                     log::warn!("[{}] Öffnen fehlgeschlagen: {:?}", self.port_name, e);
-                    tokio::time::sleep(std::time::Duration::from_secs(2)).await;
+                    sleep(Duration::from_secs(2)).await;
                 }
             }
         }
     }
 
-    async fn read_loop(&mut self, port: SerialStream) -> tokio::io::Result<()> {
-        let mut lines = FramedRead::new(port, LinesCodec::new());
-
+    async fn read_loop<S>(&mut self, mut lines: FramedRead<S, LinesCodec>) -> tokio::io::Result<()>
+    where
+        S: tokio::io::AsyncRead + Unpin,
+    {
         while let Some(line) = lines.next().await {
             match line {
                 Ok(text) => {
